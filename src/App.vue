9c764fe594fbc70e9c800a2b22773936b082d3b5
azizmashkour <azizmashkour@gmail.com>
2020-05-21 18:07:21 +0100

    feat: create main app component

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Main/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import { Footer, Header } from './components'
+import { Main } from './views'
+
+export default {
+  name: 'app',
+  components: {
+    Footer,
+    Header,
+    Main
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./assets/scss/animations";
+@import "./assets/scss/app";
+</style>
