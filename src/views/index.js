6ae3aa18ce84fc3d62a2f69810039e304087f9c7
azizmashkour <azizmashkour@gmail.com>
2020-05-21 17:56:14 +0100

    feat: combine all separated components

---
+++ src/views/index.js
@@ -0,0 +1,15 @@
+// Partials components
+import Culture from './partials/Culture.vue'
+import Progress from './partials/Progress.vue'
+import Office from './partials/Office.vue'
+import Investors from './partials/Investors.vue'
+
+import Main from './Main.vue'
+
+export {
+  Main,
+  Culture,
+  Progress,
+  Office,
+  Investors
+}
