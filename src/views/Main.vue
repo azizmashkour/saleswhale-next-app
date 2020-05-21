b4a4b837a35b773db2f9306445dacbd9477dad7d
azizmashkour <azizmashkour@gmail.com>
2020-05-21 17:55:33 +0100

    feat: create main component and import all subcomponents

---
+++ src/views/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <Culture/>
+    <Progress/>
+    <Office/>
+    <Investors/>
+  </main>
+</template>
+
+<script>
+import { Culture, Progress, Office, Investors } from './'
+export default {
+  components: {
+    Culture,
+    Progress,
+    Office,
+    Investors
+  }
+}
+</script>
