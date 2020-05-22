33c68b584bc25e3cb156de2a13fc1cf814a5b6f8
azizmashkour <azizmashkour@gmail.com>
2020-05-22 04:42:53 +0100

    feat: add footer responsive design

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -2,7 +2,7 @@
   <div class="pt-4" id="footer">
     <div class="mx-5 pb-5">
       <div class="row">
-        <div class="ml-5 col-12 col-lg-3 mb-3 mb-lg-0">
+        <div class="ml-5 col-12 col-lg-3 mb-3 mb-lg-0 col-md-3 mobile-ml-1">
           <ul class="list-unstyled list-spaced">
             <div class="row no-gutters">
               <div class="col-lg-9">
@@ -11,7 +11,7 @@
             </div>
           </ul>
         </div>
-        <div class="col-12 col-lg-2 mb-3 mb-lg-0">
+        <div class="col-6 col-lg-2 mb-3 mb-lg-0 col-md-4 ml-auto">
           <ul class="list-unstyled list-spaced">
             <li class="mb-1">
               <h5>Why Saleswhale?</h5>
@@ -36,7 +36,7 @@
             </li>
           </ul>
         </div>
-        <div class="col-12 col-lg-2 mb-3 mb-lg-0">
+        <div class="col-6 col-lg-2 mb-3 mb-lg-0 col-md-3">
           <ul class="list-unstyled list-spaced">
             <li class="mb-1">
               <h5>Product</h5>
@@ -62,7 +62,7 @@
             </div>
           </ul>
         </div>
-        <div class="col-12 col-lg-2 mb-3 mb-lg-0">
+        <div class="col-6 col-lg-2 mb-3 mb-lg-0 col-md-4 ml-auto desk-ml-auto">
           <ul class="list-unstyled list-spaced">
             <li class="mb-1">
               <h5>Company</h5>
@@ -87,7 +87,7 @@
             </li>
           </ul>
         </div>
-        <div class="col-12 col-lg-2 mb-3 mb-lg-0">
+        <div class="col-6 col-lg-2 mb-3 mb-lg-0 col-md-3">
           <ul class="list-unstyled list-spaced">
             <li class="mb-1">
               <h5>Contact</h5>
@@ -114,12 +114,20 @@
     </div>
     <div class="text-center copy-rights pt-5">
       <div class="row mx-5">
-        <div class="col-lg-6 text-left">
+        <div class="col-lg-3 ml-auto mobile-only">
+          <a href="#" class=""><img src="../../assets/images/footer/fb.svg" alt="Logo" class="socials"></a>
+          <a href="#" class="mx-3"><img src="../../assets/images/footer/tw.svg" alt="Logo" class="socials"></a>
+          <a href="#" class=""><img src="../../assets/images/footer/lk.svg" alt="Logo" class="socials"></a> <br><br>
+          <a href="#" class="font-weight-bold font-16">Privacy Policy</a> <br> <br>
+          <a href="#" class="font-weight-bold font-16">Terms of use</a>
+          <p class="mt-3 font-16">&#169; 2020 SalesWhale, Inc. All rights reserved. </p>
+        </div>
+        <div class="col-lg-6 text-left desktop-only">
           <a href="#" class="pr-3 font-weight-bold font-16">Privacy Policy</a>
           <a href="#" class="font-weight-bold font-16">Terms of use</a>
           <p class="mt-3 font-16">&#169; 2020 SalesWhale, Inc. All rights reserved. </p>
         </div>
-        <div class="col-lg-3 ml-auto">
+        <div class="col-lg-3 ml-auto desktop-only">
           <a href="#" class=""><img src="../../assets/images/footer/fb.svg" alt="Logo" class="socials"></a>
           <a href="#" class="mx-3"><img src="../../assets/images/footer/tw.svg" alt="Logo" class="socials"></a>
           <a href="#" class=""><img src="../../assets/images/footer/lk.svg" alt="Logo" class="socials"></a>
@@ -134,7 +142,36 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
-
+@media (min-width: 320px) and (max-width: 600px) {
+  #footer {
+    .mx-5 {
+      margin-left: 1rem!important;
+      margin-right: 1rem!important;
+    }
+    .ml-5 {
+      margin-left: 0!important;
+    }
+    li {
+      h5 {
+        font-size: 16px!important;
+      }
+      a {
+        font-size: 14px!important;
+      }
+    }
+    .copy-rights {
+      background-image: url(../../assets/images/footer/mobile-bg.svg)!important;
+      height: 17rem!important;
+      background-size: contain;
+    }
+    .text-center {
+      text-align: center!important;
+    }
+  }
+  .font-16 {
+    font-size: 14px!important;
+  }
+}
 #footer,
 .copy-rights {
   font-size: 12px;
