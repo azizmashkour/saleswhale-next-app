601ba3566677cbc54e3ca6a543e7be863f5b17cf
azizmashkour <azizmashkour@gmail.com>
2020-05-23 09:58:33 +0100

    test: retake snapshot

--- __tests__/Header.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Header from './src/components/shared/Header'
-
-describe('Component', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Header)
-  expect(wrapper.element).toMatchSnapshot()
-  })
-})
